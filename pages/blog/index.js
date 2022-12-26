55e71d1dba7aff51d088f9583d4f0d5559ee0bba
unknown <sifatniloy18@gmail.com>
2022-12-25 23:17:04 -0600

    link component and title

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
 
 const blog = () => {
     return (
         <div>
+            <Head>
+                <title>Blog page</title>
+            </Head>
             <h1>This is blog page</h1>
+            <Link href="/blog/blog1">
+                blog1
+            </Link>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-// const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Home page</title>
+      </Head>
+
       <h1>Hello next js</h1>
-      <p>another text</p>
+      <Link href="/blog">
+        <button>Blog</button>
+      </Link>
     </div>
   )
 }
