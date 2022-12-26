8b92ade4b6f1540a498b2280443fd78e8249548b
unknown <sifatniloy18@gmail.com>
2022-12-26 16:11:43 -0600

    dynamic link added from button

--- Components/Post/Post.js
+++ Components/Post/Post.js
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 const Post = ({post}) => {
     return (
         <div className="card mt-10 bg-blue-100 shadow-xl">
            
             <div className="card-body">
                 <h2 className="card-title">{post.title}</h2>
-                <p>{post.body}</p>
+                {/* <p>{post.body}</p> */}
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">See details</button>
+                    <Link href={`/posts/${post?.id}`}>
+                        <button className="btn btn-primary">See details</button>
+                    </Link>
                 </div>
             </div>
         </div>
---
+++ pages/posts/[postId].js
@@ -0,0 +1,44 @@
+
+const PostDetails = ({post}) => {
+    return (
+        <div className="card mx-10 my-10 bg-green-200 shadow-xl">
+            <div className="card-body items-center text-center">
+                <h2>PostId: {post?.id}</h2>
+                <h2 className="card-title">{post?.title}</h2>
+                <p>{post?.body}</p>
+                
+            </div>
+        </div>
+    );
+};
+
+export const getStaticProps = async (context) => {
+    const {params}= context;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params?.postId}`)
+    const data = await res.json()
+
+    return {
+        props: {
+            post: data
+        }
+    }
+}
+
+export const getStaticPaths= async ()=> {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts= await res.json()
+    
+    const paths= posts.map(post=>{
+        return{
+            params:{
+                postId :`${post.id}`
+            }
+        }
+    })
+
+    return{
+        paths,
+        fallback:false
+    }
+}
+export default PostDetails;
