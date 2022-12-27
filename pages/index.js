49d1ec0fcfcb34935deaca556c89558c4eb9e2f1
unknown <sifatniloy18@gmail.com>
2022-12-26 19:54:35 -0600

    adding css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Banner from "../Components/Banner/Banner";
+import Footer from "../Components/Footer/Footer";
+import Post from "../Components/Post/Post";
 
 export default function Home() {
   return (
@@ -16,13 +19,17 @@ export default function Home() {
       <br /> <br /><br /><br /><br />
       <Image src="/nature.jpg" width={600} height={400}></Image> */}
 
+      <Banner></Banner>
+      
+      <Footer></Footer>
 
-      <button className="btn">Button</button>
+
+      {/* <button className="btn">Button</button>
       <button className="btn btn-primary">Button</button>
       <button className="btn btn-secondary">Button</button>
       <button className="btn btn-accent">Button</button>
       <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <button className="btn btn-link">Button</button> */}
     </div>
   )
 }
